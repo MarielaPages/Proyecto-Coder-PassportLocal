9271427504a9f733cf6649c44852f9c33a00bcb1
Mariela Pages <mai.pages5@gmail.com>
2022-07-14 23:48:13 -0300

    levanto server y seteo middlewares, motor de plantillas

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import apiRoutes from './src/routes/apiRoutes.js'
+import passport from 'passport'
+import './src/passport/local.js'
+import './src/mongooseConnection/mongooseConnection.js'
+import { appendFile } from 'fs'
+
+//creo mi app servidor
+const app = express();
+
+//le digo a la app donde estaran mis templates y prendo el motor de plantillas
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/', apiRoutes)
+
+
+
+//inicio server
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Your app is listening on port ${PORT}`)
+})
+
+server.on('error', error => console.log(`Error en el servidor ${error}`))
